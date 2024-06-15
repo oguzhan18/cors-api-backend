6976fa6186ba42303f08c7207b4d8e92b5a27fc7
oguzhan18 <oguzhancart1@gmail.com>
2024-06-15 14:44:24 +0300

    update to main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,19 @@ async function bootstrap() {
     .addTag('proxy')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'CORS Proxy API',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/54273323?v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   await app.listen(3000);
 }
